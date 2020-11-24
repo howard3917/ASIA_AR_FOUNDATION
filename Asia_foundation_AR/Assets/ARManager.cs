9d07c3d5763492c55ca4a667f363c11ceccb4f54
howard3917 <71679856+howard3917@users.noreply.github.com>
2020-11-24 11:58:57 +0800

    foundation

---
+++ Asia_foundation_AR/Assets/ARManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class ARManager : MonoBehaviour
+{
+    [Header("點擊後要生成的物件")]
+    public GameObject obj;
+    [Header("AR 管理器")]
+    public ARRaycastManager arManager;
+
+    private Vector2 pointmouse;
+
+    private List<ARRaycastHit> hits;
+
+    private void Tap()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            pointmouse = Input.mousePosition;
+            print(pointmouse);
+
+
+        }
+    }
+    private void Update()
+    {
+            Tap();
+    }
+
+
+}
+
